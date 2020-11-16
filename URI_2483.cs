8f036bb52750858f5d4df7c29f4e643ce3e86cee
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2020-11-15 17:04:32 -0800

    2483 - Merry Christmaaas!

---
+++ URI_2483.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2483f
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Feliz nat");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("a");
+            }
+            Console.Write("l!\n");
+            Console.ReadKey();
+        }
+    }
+}
