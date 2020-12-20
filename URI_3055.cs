8f1237b9131fcc4faacaab59f8c8a7d3774d9dc8
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2020-12-19 18:47:47 -0800

    3055 - Nota Esquecida
    3055 - Nota Esquecida Solution in C#

---
+++ URI_3055.cs
@@ -0,0 +1,23 @@
+using System;
+
+    class URI
+    {
+     public  static void Main(string[] args)
+        {
+
+            int A, M, result;
+          
+          
+                A = Convert.ToInt32(Console.ReadLine());
+                M = Convert.ToInt32(Console.ReadLine());
+                result = (M * 2) - A;
+                Console.WriteLine(result);
+            
+                 Console.ReadKey();
+
+
+
+
+    }
+}
+
